0b33b7a97862022f1f350fd34dbb8203673847c9
Kavzars <alex.berrgmann95@outlook.de>
2023-06-12 22:50:39 +0200

    upload

--- backend_database_handler.py
+++ backend_database_handler.py
@@ -86,17 +86,65 @@ def write_database():
         else:
             print("\n\n")
 
-def write_liked_songs():
+def get_liked_songs():
     results = sp.current_user_saved_tracks()
+    track_id = []
      # Iteration durch die Ergebnisse und Ausgabe der Songdetails
-    for idx, item in enumerate(results['items']):
-        track = item['track']
-        track_name = track['name']
-        artist_name = track['artists'][0]['name']
-        print(f'{idx + 1}. {track_name} - {artist_name}')
+    for item in results['items']:
+        track_id_temp = item['track']['id']
+        track_id.append(track_id_temp)
+    
+    print(track_id)
+        
+    return (track_id)
+    
+
+def write_multiple_songs(song_array):
+    database = pd.read_csv("database_songs.csv")
+
+    for track in song_array:
+        track_id = sp.track(track)
+        
+    if track_id in database['track_id'].values:          
+        print("\nDer Track ist bereits in der Datenbank\n")
+    else:
+        track_info = sp.track(track_id)
+        audio_features = sp.audio_features(track_id)        # Abrufen der Audio Features -> Overall Track analyse 
+        audio_analysis = sp.audio_analysis(track_id)        # Abrufen der Section features -> Features der Signifikanten Stelle
+        sections = audio_analysis['sections']
+        sig_sec = sections[1]
+
+        data_current = pd.DataFrame({
+                            'track_id':[track],
+                            'title':[track_id['item']['name']],
+                            'artist':[artist['name'] for artist in track_id['item']['artists']],
+                            'album':[track_id['item']['album']['name']],
+                            'genres':[sp.recommendation_genre_seeds()],
+                            'release_date':[track_info['album']['release_date']],
+                            'acousticness': [audio_features[0]['acousticness']],
+                            'danceability':[audio_features[0]['danceability']],
+                            'energy':[audio_features[0]['energy']],
+                            'instrumentalness':[audio_features[0]['instrumentalness']],
+                            'key':[audio_features[0]['key']],
+                            'speechiness':[audio_features[0]['speechiness']],
+                            'tempo':[audio_features[0]['tempo']],
+                            'valence':[audio_features[0]['valence']],
+                            'sec1_tempo':[sig_sec['tempo']],                            # SECTIONS PARAMETER weiter hinzufügen. Datenbank erweitern, deklarieren als Funktion, Übergabeparameter schaffen(falls welche gebraucht werden). 
+                            'sec1_tempo_confidence':[sig_sec['tempo_confidence']],
+                            'sec1_key':[sig_sec['key']],
+                            'sec1_key_confidence':[sig_sec['key_confidence']],
+                            'sec1_mode':[sig_sec['mode']],
+                            'sec1_mode_confidence':[sig_sec['mode_confidence']],
+                            'sec1_duration':[sig_sec['duration']],
+                            'sec1_start':[sig_sec['start']],
+                            })
+        
+        database = pd.concat([database, data_current], ignore_index=True)   # Zusammenfügen der beiden DataFrames
+        database.to_csv('database_songs.csv', index = False)    #Save
 
 
 while True:         # Für mich zum handlen, Aufruf der Funktion muss über Frontend erfolgen 
-    time.sleep(10)  # 10 Sekunden pause, bevor der code erneut gerunnt wird
     write_database()
-    write_liked_songs()
+    liked_songs = get_liked_songs()
+    #write_multiple_songs(liked_songs)
+    time.sleep(10)  # 10 Sekunden pause, bevor der code erneut gerunnt wird
